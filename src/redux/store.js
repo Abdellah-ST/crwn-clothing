44a20b099d12d8d79b5a0ee948bc3b10da28dbde
Abdellah-ST <72022485+Abdellah-ST@users.noreply.github.com>
2021-10-21 04:54:01 +0100

    Removed the redux logger from non development environment

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,10 +6,10 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if(process.env.NODE_ENV === 'development'){
+if (process.env.NODE_ENV === 'development') {
     middlewares.push(logger);
-}
-
+  }
+  
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 const persistor = persistStore(store);
